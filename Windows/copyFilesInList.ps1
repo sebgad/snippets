50c8fb8f60c54998f2129afc2d2507cfd3a3d6ed
sebastian <Sebastian.Gade@gmail.com>
2020-10-28 21:43:35 +0100

    bugfix folder copy

--- Windows/copyFilesInList.ps1
+++ Windows/copyFilesInList.ps1
@@ -1,9 +1,9 @@
 ﻿# Define File List location
-# absolute Path, e.g. C:\Users\sebastian\Desktop\fileList.txt, or just filename ( then script and filename must be at the same location )
-$strFileListLoc="C:\Users\sebastian\Desktop\fileList.txt";
+# absolute Path, e.g. "C:\Users\fileList.txt", or just filename ( then script and filename must be at the same location )
+$strFileListLoc="C:\ExampleFileList.txt";
 
-# Shall files in target be overwritten (please insert $true or $false)
-$b_overwrite_Target=$true;
+# Shall files in target be overwritten? (please insert $true or $false)
+$b_overwrite_Target=$false;
 
 # Read out FileList and ignoring Lines starting with "#" (=Comment Lines)
 $lstFileList=(Get-Content -Path $strFileListLoc) -notmatch '^#';
@@ -20,6 +20,17 @@ foreach($strLine in $lstFileList){
     if (Test-Path $strSource -PathType Leaf){
         # Check if Source is a folder
         if ($strSource[-1] -eq "*"){
+            # Add slash ("\") to the target folder if not exist 
+            if (-not($strTarget[-1] -eq "\")){
+                $strTarget="$strTarget\"
+            }
+            
+            # Check if TargetFolder already exists
+            if (-not(Test-Path $strTarget -PathType Container)){
+                # Create Target Folder, Output-Text should not be displayed (Out-Null)
+                mkdir $strTarget | Out-Null;
+            }
+
             # Check if overwrite-mode is turned on
             if ($b_overwrite_Target){
                 Write-Host -ForegroundColor Green "Copy $strSource to $strTarget, overwriting files if exists";
@@ -56,3 +67,4 @@ foreach($strLine in $lstFileList){
         Write-Host -ForegroundColor Yellow "Source File Location $($strSource) does not exists!";
     }
 }
+
